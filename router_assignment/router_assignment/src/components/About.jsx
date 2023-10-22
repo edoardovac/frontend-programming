59a84c0b0d276c72145944caa7e5a457bf5b4fa6
edoardovac <113552591+edoardovac@users.noreply.github.com>
2023-10-22 17:36:44 +0300

    Delete router_assignment/router_assignment directory

--- router_assignment/router_assignment/src/components/About.jsx
+++
@@ -1,5 +0,0 @@
-export default function About() {
-    return(
-    <h3>With my app you can...</h3>
-    );
-}
--- router_assignment/router_assignment/src/components/Contact.jsx
+++
@@ -1,5 +0,0 @@
-export default function Contact() {
-    return(
-    <h3>Email: name.surname@email.com</h3>
-    );
-}
